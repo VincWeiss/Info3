182e4c29ba4612092a8a4ed8d5b06daafcb03600
OxanaDoroshkevich <oxana.doroshkevich@student.reutlingen-university.de>
2016-01-20 17:06:52 +0100

    0%2 working

--- cpp/StartMain.cpp
+++ cpp/StartMain.cpp
@@ -6,41 +6,54 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
+
 	// AUFGABE 1.1
-	cout << "main started " << endl;
 	FilterableList <string> s;
-	s.add(s.randomString());
-	s.add(s.randomString());
+	for (int i = 0; i < 7; i++) {
+		s.add(s.randomString());
+	}
+	cout << "stringList size: " << s.getSize() << endl;
+	s.filter([](string x) ->bool {return x.length() >= 3; });
+	s.filter([](string x) ->bool {return x.find('s') <= x.length(); });
+	
 	FilterableList <float> f;
-	f.add(f.numbers(3));
-	f.add(f.numbers(5));
-	f.add(f.numbers(7));
-	f.add(f.numbers(1));
-	f.add(f.numbers(2));
-	f.add(f.numbers(-5));
-	f.add(f.numbers(400));
-	f.add(-0.5f);
+	for (int j = 0; j < 7; j++) {
+		f.add(f.numbers());
+	}
+	cout << 2 % 2 << endl;
 	cout << "floatList size: " << f.getSize() << endl;
 	f.filter([](float x) ->bool {return x < 0; });
 	f.filter([](float x) ->bool {return x <= 5; });
-	f.filter([](int x) ->bool {return x % 2; });
-	cout << "stringList size: " << s.getSize() << endl;
-	s.filter([](string x) {return x.length() >= 3; });
-	s.filter([](string x) {return x.find('s'); });
+	f.filter([](float x) -> bool
+				{
+					if (x >= 0.5) {
+						x = int (x - 0.5) % 2; 
+					}
+					else if (x <= -0.5) {
+						x = int(x + 0.5) % 2;
+					}
+					else {
+						x = 1 % 2;
+					}
+					
+					return x;
+				});
 
 	//AUFGABE 1.2
+	int low = -1;
+	int high = 2;
 	MaxSumLambda ms = MaxSumLambda();
-	ms.sum([](int x) ->int {return (x*x) + 7*x; }, 2, 4);
+	ms.sum([](int x) ->int {return (x*x) + 7; }, low, high);
 
 	vector<lambda> floatLambdas = {
 		[](float x) -> float { return x - 1; },
 		[](float x) -> float { return x*x; },
-		[](float x) -> float { return x / 5; }
+		[](float x) -> float { return x / 5; },
+		[](float x) -> float { return ((x*x*x) - (x*x + 2 * x) + 27 * x - 101); }
 	};
 
-	ms.max(floatLambdas, 2.5);
+	ms.max(floatLambdas, -3.1f);
 	system("pause");
 
 	return 0;
