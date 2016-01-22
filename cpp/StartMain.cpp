58421a5fe4713a49e04344a6f69377df426577fd
OxanaDoroshkevich <oxana.doroshkevich@student.reutlingen-university.de>
2016-01-22 01:43:46 +0100

    all prints in main

--- cpp/StartMain.cpp
+++ cpp/StartMain.cpp
@@ -10,22 +10,36 @@ int main(){
 
 	// AUFGABE 1.1
 	FilterableList <string> s;
+	FilterableList <string> sRes;
 	for (int i = 0; i < 7; i++) {
 		s.add(s.randomString());
 	}
-	cout << "stringList size: " << s.getSize() << endl;
-	s.filter([](string x) ->bool {return x.length() >= 3; });
-	s.filter([](string x) ->bool {return x.find('s') <= x.length(); });
-	
+	cout << "all elements of string list : ";
+	s.printResult(s.getAll());
+	s.remove("ghum");
+	sRes = s.filter([](string x) ->bool {return x.length() >= 3; });
+	cout << "all strings with word length >= 3 : ";
+	sRes.printResult(sRes.getAll());
+	sRes = s.filter([](string x) ->bool {return x.find('s') <= x.length(); });
+	cout << "all strings with an 's' : ";
+	sRes.printResult(sRes.getAll());
+	cout << endl << endl;
+
 	FilterableList <float> f;
+	FilterableList <float> fRes;
 	for (int j = 0; j < 7; j++) {
-		f.add(f.numbers());
+		f.add(f.randomNumbers());
 	}
-	cout << 2 % 2 << endl;
-	cout << "floatList size: " << f.getSize() << endl;
-	f.filter([](float x) ->bool {return x < 0; });
-	f.filter([](float x) ->bool {return x <= 5; });
-	f.filter([](float x) -> bool
+	cout << "all elements of float list : ";
+	f.printResult(f.getAll());
+	f.remove(f.getFirst());
+	fRes = f.filter([](float x) ->bool {return x < 0; });
+	cout << "all elements < 0 : ";
+	fRes.printResult(fRes.getAll());
+	fRes = f.filter([](float x) ->bool {return x <= 5; });
+	cout << "all elements <=5 : ";
+	fRes.printResult(fRes.getAll());
+	fRes = f.filter([](float x) -> bool
 				{
 					if (x >= 0.5) {
 						x = int (x - 0.5) % 2; 
@@ -39,12 +53,17 @@ int main(){
 					
 					return x;
 				});
+	cout << "all odd elements : ";
+	fRes.printResult(fRes.getAll());
+	cout << endl << endl;
 
 	//AUFGABE 1.2
 	int low = -1;
 	int high = 2;
+	int res;
 	MaxSumLambda ms = MaxSumLambda();
-	ms.sum([](int x) ->int {return (x*x) + 7; }, low, high);
+	res = ms.sum([](int x) ->int {return (x*x) + 7; }, low, high);
+	cout << "the result of sum between " << low << " and " << high << " is " << res << endl << endl;
 
 	vector<lambda> floatLambdas = {
 		[](float x) -> float { return x - 1; },
@@ -54,6 +73,7 @@ int main(){
 	};
 
 	ms.max(floatLambdas, -3.1f);
+	//cout << "the function bla bla jetzt " << ms.max(floatLambdas, -3.1f) << endl;
 	system("pause");
 
 	return 0;
