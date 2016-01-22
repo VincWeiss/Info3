cb58bd240c16e259f63cd91a3d1461ede4134bd2
OxanaDoroshkevich <oxana.doroshkevich@student.reutlingen-university.de>
2016-01-22 01:41:59 +0100

    finisched LambdaMaxSum.cpp

--- cpp/MaxSumLambda.cpp
+++ cpp/MaxSumLambda.cpp
@@ -20,12 +20,15 @@ MaxSumLambda::~MaxSumLambda() {
 //@param function<int(int)> func, int lo, int hi
 //summs all function values to one result between the lo and hi operator 
 // return int result
-int MaxSumLambda::sum(std::function<int(int)> func, int lo, int hi) { // 5 und 10
+int MaxSumLambda::sum(std::function<int(int)> func, int lo, int hi) { 
+	if (lo > hi) {
+		throw invalid_argument("low int is higher than the high int");
+	}
+	
 	int result = 0;
 	for (int i = (lo+1); i < hi; i++) {
 		result = result + func(i);
 	}
-	cout << "the result of sum between "<< lo <<" and " << hi << " is : " << result << endl;
 	return result;
 }
 
@@ -34,16 +37,16 @@ int MaxSumLambda::sum(std::function<int(int)> func, int lo, int hi) { // 5 und 1
 //gets a vector with lambda expressions and finds the biggest float x value 
 //in each of the functions. Returns a function at index biggest x found.
 //return vector<lambda> functions
-lambda MaxSumLambda::max(vector<lambda> functions, float x) { //vector = x -1 ; x*x ; x/5 und x = 2.5
+lambda MaxSumLambda::max(vector<lambda> functions, float x) {
+
 	if (functions.size() == 0) {
-		return nullptr;
-		//return raus machen !!!
+		throw invalid_argument("Lambda vector is empty");
 	}
 
 	vector<float> results(functions.size());
 	for (int i = 0; i < functions.size(); i++) {
 		results[i] = functions[i](x);
-		cout << "Result at index " << i << " is " << (float)results[i] << endl;
+		//cout << "Result at index " << i << " is " << (float)results[i] << endl;
 	}
 
 	vector<float>::iterator maxElementIterator = max_element(results.begin(), results.end());
